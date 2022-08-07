bf81abe375d524924f8edd6473a07fccf4c04839
sSpicey <glm101@hotmail.com>
2022-08-07 17:45:40 -0300

    add navbar features

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,3 +1,4 @@
+import { Search, Notifications, ArrowDropDown } from "@material-ui/icons";
 import "./navbar.scss";
 
 const NavBar = () => {
@@ -17,6 +18,21 @@ const NavBar = () => {
 
           <span>New and Popular</span>
           <span>My List</span>
+        </div>
+        <div className="right">
+          <Search className="icon" />
+          <span>KID</span>
+          <Notifications className="icon" />
+          <img
+            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+          />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
